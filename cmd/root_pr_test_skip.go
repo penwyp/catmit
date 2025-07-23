c393b43223c234d4714bd154f35b79e82a0ffa9f
penwyp <wu.pen1120@gmail.com>
2025-07-23 19:48:59 +0800

    feat(squash): add commit message cleaning and testing
    refactor(test): consolidate test files and remove deprecated code
    test(squash): add comprehensive tests for message cleaning
    test(cmd): add new command tests and stdin message parsing

--- cmd/root_pr_test_skip.go
+++
@@ -1,7 +0,0 @@
-//go:build skip
-// +build skip
-
-package cmd
-
-// This file contains tests that need to be refactored for the new architecture
-// They are temporarily disabled with the skip build tag
